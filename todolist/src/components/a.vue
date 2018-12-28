a8c236ba490c94c50b6b4a127b1f6e4f4bbab5bb
Evalyq <965661503@qq.com>
2018-12-28 08:06:35 +0800

    '提交'

---
+++ todolist/src/components/a.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <input
+            type="checkbox"
+            v-bind:checked="checked"
+            v-on:change="$emit('change', $event.target.checked)"
+            >
+            <div class="qui-list">
+
+                <span class="list-tips">{{tipsText}}</span>
+
+                <!-- <qui-btn v-on:btnClickEvent='btnClickEvent1' :msg=msg class='small'></qui-btn> -->
+
+                <component :is='currentView' v-on:btnClickEvent='clickEvent' :msg=msg class="small" keep-alive></component>
+
+            </div>
+        <div @click="handleFun">{{message}}</div>
+    </div>
+</template>
+<script>
+export default {
+    model: {
+        prop: 'checked',
+        event: 'change'
+    },
+    props: {
+        checked: Booleanm
+        msg:{
+
+            default:'上传'
+
+        },
+
+        tipsText:{
+
+            default: '默认的文案1'
+
+        },
+
+        currentView:{
+
+            default: 'qui-btn'
+
+        }
+    },
+    data(){
+        return {
+            message:'sdsfs'
+        }
+    },
+    methods:{
+        handleFun(){
+            this.$emit("myfu",this,message)
+        },
+        clickEvent:function(){
+
+				alert('...')
+
+			}
+    }
+}
+</script>
---
+++ todolist/src/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <abc @myfu="myfu"></abc>
+        <myComponent :show.sync='valueChild' style="padding: 30px 20px 30px 5px;border:1px solid #ddd;margin-bottom: 10px;"></myComponent>
+        <button @click="changeValue">toggle</button>
+    </div>
+</template>
+<script>
+    import abc from '../components/a.vue'
+import myComponent from '../components/b.vue'
+export default {
+    data(){
+        return{
+            valueChild:true,
+        }
+    },
+    components:{
+        abc
+    },
+    methods:{
+        myfu (message) {
+            console.log(message)
+        },
+        changeValue(){
+            this.valueChild = !this.valueChild
+        }
+    }
+}
+</script>
